260426b12dc63a5f6609dbd580af43725c7352fe
Henrique Custodia <henrique.custodia@angular-ecommerce.com>
2019-09-13 23:09:08 -0300

    adicionado GA

--- site/.vuepress/config.js
+++ site/.vuepress/config.js
@@ -3,12 +3,21 @@ module.exports = {
     description: 'Posts sobre minha experiência e o mundo Javascript',
     lang: 'pt-BR',
     base: '/blog/',
-    plugins: {
-        'seo': {
-            url: (_, $site, path) => ($site.themeConfig.domain || '') + '/blog' + path,
-            image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + '/blog' + $page.frontmatter.image),
-        }
-    },
+    plugins: [
+        [
+            'seo',
+            {
+                url: (_, $site, path) => ($site.themeConfig.domain || '') + '/blog' + path,
+                image: ($page, $site) => $page.frontmatter.image && (($site.themeConfig.domain || '') + '/blog' + $page.frontmatter.image),
+            }
+        ],
+        [
+            '@vuepress/google-analytics',
+            {
+                'ga': 'UA-147966506-1'
+            }
+        ]
+    ],
     themeConfig: {
         author: {
             name: 'Henrique Custódia',
